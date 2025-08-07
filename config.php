bc88bfc2b9753b0c1fd8713d73006de740635aa6
zapp90 <159251440+zapp90@users.noreply.github.com>
2025-08-07 19:05:42 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'db_host'     => 'localhost',
+    'db_user'     => 'root',
+    'db_pass'     => '',
+    'databases'   => ['mydatabase1', 'mydatabase2'], // list of DBs
+    'backup_dir'  => __DIR__ . '/backups',
+    'compress'    => true
+];
